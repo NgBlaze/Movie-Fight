2743bf8261bda20413357d7ee090a2e499484a8e
Precious <okolaaprecious@gmail.com>
2022-11-22 01:21:19 +0100

    cc

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "b1502f3f",
         s: searchTerm,
@@ -48,7 +48,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "b1502f3f",
       i: movie.imdbID,
